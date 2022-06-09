6e29033bf5eaf49185330add9af488dd69d3c663
Regis Mazur <regis_mazur@hotmail.com>
2022-06-09 11:50:24 +0100

    Update ground.js

--- PRO-C21 Crumpled paper balls/ground.js
+++ PRO-C21 Crumpled paper balls/ground.js
@@ -1,13 +1,3 @@
-const Bodies = Matter.Bodies;
-const World = Matter.World;
-
-let groundplane;
-
-var groundbase_options = {
-  isStatic: true,
-  render: { fillStyle: '#FFFF00' }
-};
-
 class ground {
   constructor(x, y, width, height) {
     this.x = x,
@@ -16,13 +6,9 @@ class ground {
       this.height = height
   }
 
-
   display() {
-
     groundplane = Bodies.rectangle(this.x, this.y, this.width, this.height, groundbase_options);
     World.add(world, groundplane);
-
     rect(this.x, this.y, this.width, this.height);
-
   }
 }
